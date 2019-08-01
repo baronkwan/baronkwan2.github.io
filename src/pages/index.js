73ee1ce45f14c76b3ab9f42d717d19d82f1103ed
Baron <baronkwan@outlook.com>
2019-08-01 22:46:04 +0800

    Add TypeIt component on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,40 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import TypeIt from "typeit"
+
+import indexStyles from "./index.module.scss"
 
 import Layout from "../components/layout"
-import Head from '../components/head'
+import Head from "../components/head"
+
+const Type = props => {
+  const typeRef = useRef(null)
+  useEffect(() => {
+    new TypeIt(typeRef.current, props).go()
+  }, [typeRef])
+
+  return <span ref={typeRef}>{props.children}</span>
+}
 
 const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hello.</h1>
-      <h2>I'm Baron, a full-stack developer living in Hong Kong!</h2>
+      <h1 className={indexStyles.greeting}>Hello.</h1>
+      <div className={indexStyles.typeWrapper}>
+        <h2>
+          This is Baron. I'm a
+          <Type
+            strings={["developer.", "data science enthusiast."]}
+            loop={true}
+            nextStringDelay={2000}
+            loopDelay={4500}
+            breakLines={false}
+            waitUntilVisible={true}
+            speed={80}
+          />
+        </h2>
+      </div>
     </Layout>
   )
-} 
+}
 export default IndexPage
